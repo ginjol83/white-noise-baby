30995b099f274a5a6ce25b0b37ec03d172255a38
agimenez <andres.gimenez@martico.com>
2023-06-14 14:46:48 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'white.noise.baby',
+  appName: 'white-noise-baby',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
